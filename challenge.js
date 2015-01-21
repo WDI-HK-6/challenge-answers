7dbc24165bfa5e4f50aca4b4a042c90ecbc6ccf4
gduck <dale.github@thefoos.net>
2015-01-21 10:55:05 +0800

    challenge 2

--- challenge.js
+++ challenge.js
@@ -1,4 +1,5 @@
 // CHALLENGE 1
+console.log('>>>>>>>>>>CHALLENGE 1');
 
 var bigDiff = function(array) {
 
@@ -23,13 +24,35 @@ var bigDiff = function(array) {
 // validation didn't work
 console.log(bigDiff([9,20,11,12]));
 
-console.log('>>>>>>>>>>CHALLENGE 1');
 
 
 
 // CHALLENGE 2
-
 console.log('>>>>>>>>>>CHALLENGE 2');
+
+var averageMedian = function(array) {
+  var length = array.length;
+  if (length == 1) {
+    return array[0];
+  }
+  var sum;
+  var average;
+  // even
+  if ((length % 2) == 0) {
+    // if even the element numbers will be (length / 2) and 
+    // (length / 2) - 1
+    sum = array[length/2] + array[(length/2)-1];
+    average = sum / 2;
+  } else {
+    // if odd, element numbers will be (floor of length / 2), 
+    // that value less one, and that value plus one
+    var middleElement = Math.floor(length / 2);
+    sum = array[middleElement - 1] + array[middleElement] + array[middleElement + 1];
+    average = sum / 3;
+  }
+  return average;
+}
+  console.log(averageMedian([9, 8, 2, 4, 7]));
 
 
 
