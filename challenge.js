cacf0525fa1371564accdd3c5f966566697ed87d
Denis Cheung <d.s.h.cheung@gmail.com>
2015-01-21 10:26:11 +0800

    challenge 1

--- challenge.js
+++ challenge.js
@@ -1,8 +1,12 @@
 // CHALLENGE 1
-
 console.log('>>>>>>>>>>CHALLENGE 1');
 
+var bigDiff = function (array) {
+  array2 = array.sort(function(a,b){return b-a})
+  return array2[0] - array2[1]
+}
 
+console.log('bigDiff[23,2,522,52,1,2,8] >>>', bigDiff([23,2,522,52,1,2,8]))
 
 // CHALLENGE 2
 
